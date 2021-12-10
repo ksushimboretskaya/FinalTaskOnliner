63dbe28bf816bf9df309863a536340ba2a8daa0b
Ksusha Shimboretskaya <Anastasia.Baruta@elmosoft.net>
2021-12-10 18:32:06 +0300

    Update exception in ElementFactoryImpl.java

--- src/main/java/elements/ElementFactoryImpl.java
+++ src/main/java/elements/ElementFactoryImpl.java
@@ -1,8 +1,5 @@
 package elements;
 
-
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 
 import java.lang.reflect.InvocationTargetException;
@@ -10,7 +7,6 @@ import java.lang.reflect.InvocationTargetException;
 import static java.text.MessageFormat.format;
 
 public class ElementFactoryImpl implements ElementFactory {
-    private final Logger logger = LogManager.getLogger("Element factory logger");
 
     @Override
     public <E extends CustomElement> E create(final Class<E> elementClass, final WebElement wrappedElement) {
@@ -18,18 +14,8 @@ public class ElementFactoryImpl implements ElementFactory {
             return findImplementationFor(elementClass)
                     .getDeclaredConstructor(WebElement.class)
                     .newInstance(wrappedElement);
-        } catch (InstantiationException e) {
-            logger.info("Error with exception: InstantiationException");
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            logger.info("Error with exception: IllegalAccessException");
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            logger.info("Error with exception: InvocationTargetException");
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-            logger.info("Error with exception: NoSuchMethodException");
-            throw new RuntimeException(e);
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            throw new RuntimeException("Error:" + e.getMessage());
         }
     }
 
@@ -37,8 +23,7 @@ public class ElementFactoryImpl implements ElementFactory {
         try {
             return (Class<? extends E>) Class.forName(format("{0}.{1}Impl", getClass().getPackage().getName(), elementClass.getSimpleName()));
         } catch (ClassNotFoundException e) {
-            logger.info("Error with exception: ClassNotFoundException");
-            throw new RuntimeException(e);
+            throw new RuntimeException("ClassNotFounderException: " + e.getMessage());
         }
     }
 }
